ed9ad87f4889d49c254e9d55c9d7e124e4d52ade
SeredaOleksandr <154976460+SeredaOleksandr@users.noreply.github.com>
2024-04-28 16:25:11 +0300

    Correct task6

--- js/task-6.js
+++ js/task-6.js
@@ -17,31 +17,25 @@ destroyBtn.addEventListener('click', destroyBoxes);
 function createBoxes() {
   const amount = Number(input.value);
 
-  if (amount < 1 || amount > 100 || !Number.isInteger(amount)) {
+  if (isNaN(amount) || amount < 1 || amount > 100) {
     alert('Please enter a number between 1 and 100');
     return;
   }
 
   const boxesDiv = document.getElementById('boxes');
-  boxesDiv.innerHTML = '';
-
-  const boxes = [];
+  const fragment = document.createDocumentFragment();
 
   for (let i = 0; i < amount; i++) {
-    const box = document.createElement('div');
     const size = 30 + i * 10;
-
+    const box = document.createElement('div');
     box.style.width = `${size}px`;
     box.style.height = `${size}px`;
-    box;
     box.style.backgroundColor = getRandomHexColor();
-
-    boxes.push(box);
+    fragment.appendChild(box);
   }
 
-  boxes.forEach(box => {
-    boxesDiv.appendChild(box);
-  });
+  boxesDiv.innerHTML = '';
+  boxesDiv.appendChild(fragment);
 
   input.value = '';
 }
